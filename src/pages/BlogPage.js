d9367fc5aa3443a84c392221fee042335b409ea8
izayed9 <izayed9@gmail.com>
2023-05-11 12:13:00 +0600

    fixed aside bar and desigin fixed

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,15 +1,19 @@
 import { Helmet } from 'react-helmet-async';
 // @mui
-import { Button, Container, Stack, Typography } from '@mui/material';
+import { Grid, Button, Container, Stack, Typography } from '@mui/material';
 // components
+import Iconify from '../components/iconify';
+import { BlogPostCard, BlogPostsSort, BlogPostsSearch } from '../sections/@dashboard/blog';
+// mock
+import POSTS from '../_mock/blog';
 
 // ----------------------------------------------------------------------
 
-// const SORT_OPTIONS = [
-//   { value: 'latest', label: 'Latest' },
-//   { value: 'popular', label: 'Popular' },
-//   { value: 'oldest', label: 'Oldest' },
-// ];
+const SORT_OPTIONS = [
+  { value: 'latest', label: 'Latest' },
+  { value: 'popular', label: 'Popular' },
+  { value: 'oldest', label: 'Oldest' },
+];
 
 // ----------------------------------------------------------------------
 
@@ -25,16 +29,21 @@ export default function BlogPage() {
           <Typography variant="h4" gutterBottom>
             Blog
           </Typography>
-          <Button variant="outlined" >
+          <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />}>
             New Post
           </Button>
         </Stack>
 
         <Stack mb={5} direction="row" alignItems="center" justifyContent="space-between">
-
+          <BlogPostsSearch posts={POSTS} />
+          <BlogPostsSort options={SORT_OPTIONS} />
         </Stack>
 
-
+        <Grid container spacing={3}>
+          {POSTS.map((post, index) => (
+            <BlogPostCard key={post.id} post={post} index={index} />
+          ))}
+        </Grid>
       </Container>
     </>
   );
